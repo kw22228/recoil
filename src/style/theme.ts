c4e0262cd5e9b694bda8dda438ca69f79c1ca480
김재원 <kw2282222@gmail.com>
2022-06-12 23:03:22 +0900

    init: theme provider

---
+++ src/style/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+    bgColor: '#f4f4f4',
+    textColor: 'navy',
+    accentColor: '#e2720c',
+};
+
+export const darkTheme: DefaultTheme = {
+    bgColor: 'navy',
+    textColor: '#f4f4f4',
+    accentColor: '#e2720c',
+};
