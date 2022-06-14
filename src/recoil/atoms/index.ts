ae07c04e32b9e17cc8e69c4679311d6a153e057d
kw22228 <hot5521@naver.com>
2022-06-14 17:35:52 +0900

    add: enum types

--- src/recoil/atoms/index.ts
+++ src/recoil/atoms/index.ts
@@ -1,7 +1,14 @@
+//atoms -> recoil의 bubble (글로벌에서 사용할 수 있는 스토어)
+
 import { atom } from 'recoil';
-import { ITodo } from '../../components/TodoList/types';
+import { Categories, ITodo } from '../../components/TodoList/types';
 
 export const todoAtom = atom<ITodo[]>({
     key: 'todo',
     default: [],
+});
+
+export const categoryAtom = atom<Categories>({
+    key: 'category',
+    default: Categories.TODO,
 });
