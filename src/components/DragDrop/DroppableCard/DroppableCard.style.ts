94c6fef65d86023205b9c83e901b96e075c0cf11
김재원 <kw2282222@gmail.com>
2022-06-16 23:28:05 +0900

    add: cross move draggable

--- src/components/DragDrop/DroppableCard/DroppableCard.style.ts
+++ src/components/DragDrop/DroppableCard/DroppableCard.style.ts
@@ -1,17 +1,5 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.main`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-`;
-
-export const Boards = styled.div`
-    display: grid;
-    grid-template-columns: repeat(1, 1fr);
-`;
-
 export const Board = styled.div`
     border: 1px solid ${props => props.theme.boardColor};
     background-color: ${props => props.theme.bgColor};
@@ -19,8 +7,19 @@ export const Board = styled.div`
     min-height: 300px;
     padding: 0 10px;
     padding-top: 30px;
-    border-radius: 30px;
+
     box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px,
         rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
         rgba(0, 0, 0, 0.09) 0px -3px 5px;
+`;
+
+export const CardHeader = styled.div`
+    text-align: center;
+    padding: 10px 30px;
+    font-weight: bold;
+`;
+
+export const Wrapper = styled.div`
+    background-color: lightgray;
+    border-radius: 30px;
 `;
--- src/components/DragDrop/DroppableCard/DroppableCard.tsx
+++ src/components/DragDrop/DroppableCard/DroppableCard.tsx
@@ -1,37 +1,29 @@
 import * as s from './DroppableCard.style';
-import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
-import { useRecoilState } from 'recoil';
-import { dragDropAtom } from '../../../recoil/atoms';
+import { Droppable } from 'react-beautiful-dnd';
 import DraggableCard from '../DraggableCard/DraggableCard';
+import React from 'react';
 
-const DroppableCard = () => {
-    const [todos, setTodos] = useRecoilState(dragDropAtom);
-    const onDragEnd = ({ destination, source, draggableId }: DropResult) => {
-        setTodos(prevTodos => {
-            const newTodos = prevTodos.filter((todo, index) => source.index !== index);
-            newTodos.splice(Number(destination?.index), 0, draggableId);
+interface IProps {
+    todoList: string[];
+    id: string;
+}
 
-            return newTodos;
-        });
-    };
+const DroppableCard = ({ todoList, id }: IProps) => {
     return (
         <s.Wrapper>
-            <DragDropContext onDragEnd={onDragEnd}>
-                <s.Boards>
-                    <Droppable droppableId="todos">
-                        {provided => (
-                            <s.Board ref={provided.innerRef} {...provided.droppableProps}>
-                                {todos.map((todo: string, index: number) => (
-                                    <DraggableCard todo={todo} key={todo} index={index} />
-                                ))}
+            <s.CardHeader>{id}</s.CardHeader>
+            <Droppable droppableId={id}>
+                {provided => (
+                    <s.Board ref={provided.innerRef} {...provided.droppableProps}>
+                        {todoList.map((todo: string, index: number) => (
+                            <DraggableCard todo={todo} key={todo} index={index} />
+                        ))}
 
-                                {/* Draggable을 움직일때, 밑에 height가 줄어드는걸 방지한다. */}
-                                {provided.placeholder}
-                            </s.Board>
-                        )}
-                    </Droppable>
-                </s.Boards>
-            </DragDropContext>
+                        {/* Draggable을 움직일때, 밑에 height가 줄어드는걸 방지한다. */}
+                        {provided.placeholder}
+                    </s.Board>
+                )}
+            </Droppable>
         </s.Wrapper>
     );
 };
