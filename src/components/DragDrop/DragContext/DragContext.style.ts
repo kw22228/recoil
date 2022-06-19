8ce0989c098921b334c676d1c86601ef54a66816
김재원 <kw2282222@gmail.com>
2022-06-19 20:19:05 +0900

    update: box shadow

--- src/components/DragDrop/DragContext/DragContext.style.ts
+++ src/components/DragDrop/DragContext/DragContext.style.ts
@@ -5,6 +5,10 @@ export const Wrapper = styled.main`
     justify-content: center;
     align-items: center;
     height: 100vh;
+
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px,
+        rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
+        rgba(0, 0, 0, 0.09) 0px -3px 5px;
 `;
 
 export const Boards = styled.div`
